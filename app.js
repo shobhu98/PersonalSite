2fbbbf24ebaf5226e3cb8bc299bac63e077f929a
shobhu98 <46121289+shobhu98@users.noreply.github.com>
2019-07-02 15:38:36 +0530

    a

---
+++ app.js
@@ -0,0 +1,16 @@
+let cy=document.body;
+
+
+ arrcolor=['#039be5','#00FF00','#000000','#FFDF00'];
+function colorchanger() {
+
+
+         let colo= arrcolor.pop();
+        cy.style.backgroundColor=colo;
+        arrcolor.unshift(colo);
+
+
+}
+
+
+setInterval(colorchanger,2000);
